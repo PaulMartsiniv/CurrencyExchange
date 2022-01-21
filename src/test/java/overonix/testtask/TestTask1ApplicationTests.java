291701c14e09f4071b5a6229762ccdf60292f396
Pavlo_Martsiniv <imitator2010@gmail.com>
2022-01-22 00:59:29 +0200

    add files

---
+++ src/test/java/overonix/testtask/TestTask1ApplicationTests.java
@@ -0,0 +1,13 @@
+package overonix.testtask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestTask1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
